56065c0d0452b340944ffde2a2b9e6f6edfe22a0
G1Joshi <joshijeevan98@gmail.com>
2021-06-20 16:38:46 +0530

    add A

---
+++ Contests/JUNE21C/COCONUT.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int xa, xb, Xa, Xb;
+        cin >> xa >> xb >> Xa >> Xb;
+        cout << Xa / xa + Xb / xb << "\n";
+    }
+    return 0;
+}
